d9962eac5117e759f407b432c7210bcda6ce848b
uiop <uiop@wasdhjkl.xyz>
2026-02-04 17:52:50 -0500

    feat: hello world via socket

--- src/agent.c
+++ src/agent.c
@@ -1,7 +1,90 @@
 #include <stdio.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+int create_socket(const char *host, uint16_t port)
+{
+	int sock, ret;
+	struct sockaddr_in addr;
+
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock < 0) {
+		perror("socket");
+		return -1;
+	}
+
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+
+	ret = inet_pton(AF_INET, host, &addr.sin_addr);
+	if (ret <= 0) {
+		perror("inet_pton");
+		close(sock);
+		return -1;
+	}
+
+	ret = connect(sock, (struct sockaddr *)&addr, sizeof(addr));
+	if (ret < 0) {
+		perror("connect");
+		close(sock);
+		return -1;
+	}
+
+	return sock;
+}
+
+int destroy_socket(int sock)
+{
+	int ret;
+
+	if (sock < 0) {
+		return -1;
+	}
+
+	ret = shutdown(sock, SHUT_RDWR);
+	if (ret < 0) {
+		perror("shutdown");
+		close(sock);
+		return -1;
+	}
+
+	ret = close(sock);
+	if (ret < 0) {
+		perror("close");
+		return -1;
+	}
+
+	return 0;
+}
 
 int main(void)
 {
-	printf("hello world\n");
+	int sk, ret;
+	ssize_t wr_bytes;
+	const char msg[] = "hello, world!";
+
+	sk = create_socket("10.10.10.20", 7777);
+	if (sk < 0) {
+		return 1;
+	}
+
+	wr_bytes = write(sk, msg, strlen(msg));
+	if (wr_bytes < 0) {
+		perror("write");
+		close(sk);
+		return 1;
+	}
+
+	ret = destroy_socket(sk);
+	if (ret < 0) {
+		return 1;
+	}
+
 	return 0;
 }
--- src/c2.c
+++ src/c2.c
@@ -1,7 +1,100 @@
 #include <stdio.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <string.h>
+
+int create_socket(uint16_t port)
+{
+	int sock, ret;
+	struct sockaddr_in addr;
+
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock < 0) {
+		perror("socket");
+		return -1;
+	}
+
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+	addr.sin_addr.s_addr = INADDR_ANY;
+
+	ret = bind(sock, (struct sockaddr *)&addr, sizeof(addr));
+	if (ret < 0) {
+		perror("bind");
+		close(sock);
+		return -1;
+	}
+
+	ret = listen(sock, SOMAXCONN);
+	if (ret < 0) {
+		perror("listen");
+		close(sock);
+		return -1;
+	}
+
+	return sock;
+}
+
+int destroy_socket(int sock)
+{
+	int ret;
+
+	if (sock < 0) {
+		return -1;
+	}
+
+	ret = shutdown(sock, SHUT_RDWR);
+	if (ret < 0) {
+		perror("shutdown");
+		close(sock);
+		return -1;
+	}
+
+	ret = close(sock);
+	if (ret < 0) {
+		perror("close");
+		return -1;
+	}
+
+	return 0;
+}
 
 int main(void)
 {
-	printf("hi world\n");
+	int sk_listen, sk_accept, ret;
+	ssize_t rd_bytes;
+	char buf[64];
+
+	sk_listen = create_socket(7777);
+	if (sk_listen < 0) {
+		return 1;
+	}
+
+	sk_accept = accept(sk_listen, NULL, NULL);
+	if (sk_accept < 0) {
+		perror("accept");
+		close(sk_listen);
+		return 1;
+	}
+
+	rd_bytes = read(sk_accept, buf, sizeof(buf));
+	if (rd_bytes < 0) {
+		perror("read");
+		close(sk_accept);
+		close(sk_listen);
+		return 1;
+	}
+
+	printf("%s\n", buf);
+
+	ret = destroy_socket(sk_listen);
+	if (ret < 0) {
+		return 1;
+	}
+
 	return 0;
 }
