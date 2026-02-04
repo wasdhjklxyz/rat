c8d5916e91b9c087ef028da536cde7c5c594e9e3
uiop <uiop@wasdhjkl.xyz>
2026-02-04 16:11:50 -0500

    build: use CMake

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,12 @@ set -euo pipefail
 
 NAME="${LAB_DOCKER_IMG_NAME:?not in devshell}"
 
-docker run --rm -v "$(pwd)":/src -w /src "$NAME" gcc "$@"
+BUILD_TYPE=""
+case "${1:-}" in
+  debug)      BUILD_TYPE="-DCMAKE_BUILD_TYPE=Debug" ;;
+  release|"") BUILD_TYPE="-DCMAKE_BUILD_TYPE=Release" ;;
+  *)          echo "Usage: build [debug|release]" >&2; exit 1 ;;
+esac
+
+docker run --rm -v "$(pwd)":/src -w /src "$NAME" \
+  sh -c "cmake -B build $BUILD_TYPE && cmake --build build"
--- scripts/mkdocker.sh
+++ scripts/mkdocker.sh
@@ -6,7 +6,7 @@ IMG="${LAB_DOCKER_IMG:?not in devshell}"
 NAME="${LAB_DOCKER_IMG_NAME:?not in devshell}"
 
 if ! docker image inspect "$NAME" &>/dev/null; then
-  docker run --name "$NAME-tmp" -t "$IMG" dnf install -y gcc gcc-c++ make
+  docker run --name "$NAME-tmp" -t "$IMG" dnf install -y gcc gcc-c++ cmake
   docker commit "$NAME-tmp" "$NAME"
   docker rm "$NAME-tmp"
 fi
