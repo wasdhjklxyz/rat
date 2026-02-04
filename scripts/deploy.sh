549fb114320c51bd66ebe80b2b6269897be15f32
uiop <uiop@wasdhjkl.xyz>
2026-02-04 17:19:21 -0500

    fix(deploy): force success

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,6 +13,7 @@ deploy() {
   [[ -f "$SSH_KEY" ]] && scp_cmd+=(-i "$SSH_KEY")
   [[ -f "$out" ]] && "${scp_cmd[@]}" "$out" "$USER@$2:~/"
   [[ -f "${out}_debug" ]] && "${scp_cmd[@]}" "${out}_debug" "$USER@$2:~/"
+  return 0
 }
 
 deploy "agent" "$TARGET"
